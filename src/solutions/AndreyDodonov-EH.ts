75446b2a29d9ed1dec6b2baa186d58e15a0e1c3a
Andrey Dodonov <aadodonov@gmail.com>
2025-04-01 17:37:58 +0200

    Comment out additional transaction fails, its logic is incorrect

--- src/solutions/AndreyDodonov-EH.ts
+++ src/solutions/AndreyDodonov-EH.ts
@@ -9,7 +9,6 @@ import type {
 } from "../types"
 
 let blocks: Map<string, string> = new Map();
-let invalidatedTransactions: Map<string, string> = new Map();
 
 function isAncestorOfOrSelf(crt: string, maybeAncestor: string): boolean {
   if (crt === maybeAncestor) {
@@ -52,26 +51,22 @@ function emitSettled(api: API, outputApi: OutputAPI, blockHash: string, transact
   outputApi.onTxSettled(transaction, info);
 }
 
-function emitSettledInvalid(api: API, outputApi: OutputAPI, blockHash: string, transaction: string) 
-{
-  let info: Settled = {blockHash, type: "invalid"};
-  outputApi.onTxSettled(transaction, info);
-  if (invalidatedTransactions.has(transaction)) {
-    const crtList = transactions.get(transaction);
-    if (crtList == undefined) throw "";
-    crtList.push(blockHash);
-    transactions.set(transaction, crtList);
-  } else {
-    const newList = [];
-    newList.push(blockHash);
-    transactions.set(transaction, newList);
-  }
-}
-
-function emitDoneInvalid(api: API, outputApi: OutputAPI, blockHash: string, transaction: string) 
-{
-  
-}
+// let invalidatedTransactions: Map<string, string> = new Map();
+// function emitSettledInvalid(api: API, outputApi: OutputAPI, blockHash: string, transaction: string) 
+// {
+//   let info: Settled = {blockHash, type: "invalid"};
+//   outputApi.onTxSettled(transaction, info);
+//   if (invalidatedTransactions.has(transaction)) {
+//     const crtList = transactions.get(transaction);
+//     if (crtList == undefined) throw "";
+//     crtList.push(blockHash);
+//     transactions.set(transaction, crtList);
+//   } else {
+//     const newList = [];
+//     newList.push(blockHash);
+//     transactions.set(transaction, newList);
+//   }
+// }
 
 function emitDone(api: API, outputApi: OutputAPI, blockHash: string, transaction: string) {
   let info: Settled;
@@ -95,16 +90,17 @@ export default function AndreyDodonov_EH(api: API, outputApi: OutputAPI) {
       const blockTransactions = api.getBody(blockHash);
 
       // check for invalid ones
-      // ToDo: we also need to check if it got invalid in one of the parents 
-      transactions.forEach((blockHashes, transaction) => {
-        for (let i=0;i<blockHashes.length; i++) {
-          if (!blockTransactions.includes(transaction)) {
-            if (!api.isTxValid(blockHash, transaction)) {
-              emitSettledInvalid(api, outputApi, blockHash, transaction);
-            }
-          }
-        }
-      });
+      // ToDo: we also need to check if it got invalid in one of the parents?
+      // but then it should stay invalid and worst case we emit again
+      // transactions.forEach((blockHashes, transaction) => {
+      //   for (let i=0;i<blockHashes.length; i++) {
+      //     if (!blockTransactions.includes(transaction)) {
+      //       if (!api.isTxValid(blockHash, transaction)) {
+      //         emitSettledInvalid(api, outputApi, blockHash, transaction);
+      //       }
+      //     }
+      //   }
+      // });
       
 
       for (let i =0; i< blockTransactions.length; i++) {
